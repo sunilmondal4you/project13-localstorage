c76c3c1da71d6d9702467d8fc2e40e745a45f517
viveking <sunilmondal4you@gmail.com>
2016-12-27 11:17:21 +0530

    new changes

--- app.js
+++ app.js
@@ -16,6 +16,20 @@ app.get('/a',function(req,res) {
     res.send("Hello TDD! can u here me?");
 });
 
+app.get('/a/b',function(req,res) {
+    res.send("Hello in the World of team Affixus!")
+});
+
+app.get('/a/b/c',function(req,res) {
+    res.send("We are team Affixus.")
+});
+
+app.get('/a/b/c/d',function(req,res) {
+    res.send("Affixus provide next gen Web programing.")
+});
+
+
+
 app.listen(3001,function () {
-    console.log("Server 3002 is started");
+    console.log("Server 3001 is started");
 });
--- public/sunil/js/ctrl.js
+++ public/sunil/js/ctrl.js
@@ -8,33 +8,79 @@ app.service('myservices',function ($localStorage) {
 
 });
 
-app.controller("default", function($scope, $localStorage) {
+app.controller("default", function($scope, $localStorage, $http) {
     
     $scope.save = function() {
         $localStorage.message = "Hello World";
-    }
+    };
 
     $scope.load = function() {
         $scope.data = $localStorage.message;
-    }
+    };
+
+
+    $scope.serverCall1 = function() {
+        
+        $http.get("/a")
+        .success(function(resdata){
+            $scope.ARES = resdata;
+        }).error(function(resdata){
+            console.log(resdata);
+        });
+        
+    };
+
+    $scope.serverCall2 = function() {
+        $http.get("/a/b")
+            .success(function(rescall){
+                $scope.RESCALL=rescall;
+            }).error(function(rescall) {
+            console.log(rescall);
+        });
+    };
+
+    $scope.serverCall3=function(){
+       $http.get("/a/b/c")
+           .success(function(call3){
+               $scope.CALL(call3);
+           }).error(function(call3){
+           console.log(call3);
+       });
+    };
+
+    $scope.serverCall4=function(){
+        
+        
+        $http.get("/a/b/c/d")
+            .success(function(call4) {
+                $scope.CALL4(call4);
+            }).error(function(call4) {
+            console.log("You can not get call from server")
+        });
+    };
+
+
 
 });
 
-app.controller("form", function($scope,$localStorage,myservices,$){
+app.controller("form", function($scope,$localStorage,myservices){
 
     $scope.user={};
-    myservices.userarry.push($scope.user);
+
 
     $scope.submitdata=function() {
         debugger;
-        localStorage.setItem('name', JSON.stringify(myservices.userarry.name));
+        myservices.userarry.push($scope.user);
+        localStorage.setItem('name', JSON.stringify($scope.user.name));
+        localStorage.setItem('email', JSON.stringify($scope.user.email));
 
-      //localStorage.name=myservices.userarry.name;
-      localStorage.email=myservices.userarry.email;
+        
   };
 
 
 });
+
+
 
 app.controller("storage", function($scope, $localStorage) {
 
